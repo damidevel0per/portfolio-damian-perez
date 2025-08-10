e1e0bd2ab118e0e2611171c2c1f25b8a9c4531d1
damidevel0per <damidevtech@gmail.com>
2025-08-10 19:23:05 -0300

    actualizando evento al hacer click en el botón enviar del formulario contacto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
+//ANIMACION HERO
 let presentacionHero = document.getElementById('presentacionHero');
 let presentacion = 'Desarrollador viviendo en Entre Ríos, Argentina.';
 let velocidad = 50;
 let i = 0; 
-
+//Esta funcion recorre el string presentacion y lo va insertando en el html del hero
 function animacionEscribir(){
     
     if(i < presentacion.length){
@@ -12,5 +13,16 @@ function animacionEscribir(){
     }
 }
 
-
+//Acá se invoca la funcion para animar el texto del hero
 animacionEscribir();
+
+//ALERTA AL ENVIAR INFORMACION EN FORMULARIO CONTACTO
+let btnEnviar = document.getElementById('btnEnviar');
+
+//Evento al hacer click en enviar en el formulario de contacto
+btnEnviar.addEventListener('click', mensajeAlContactarse)
+
+//Funcion de alerta
+function mensajeAlContactarse(){
+    alert('Ups 😓 Esta funcionalidad todavia no está disponible')
+}
