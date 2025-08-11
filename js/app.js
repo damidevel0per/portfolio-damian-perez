7d7cce785785548ca626a0846991f39585e58159
damidevel0per <damidevtech@gmail.com>
2025-08-10 22:23:59 -0300

    agregando funcion de cambiar el tema de oscuro a claro con boton en el header

--- js/app.js
+++ js/app.js
@@ -1,4 +1,73 @@
-//ANIMACION HERO
+//------CAMBIAR DE TEMA LA PAGINA-------
+
+let colorPagina = document.getElementById('colorPagina')
+let body = document.body;
+let inicio = document.getElementsByClassName('hero')[0]
+let letrasHero = document.getElementsByClassName('info-hero')
+let proyectos1 = document.getElementsByClassName('proyectos')[0]
+let proyectos2 = document.getElementsByClassName('tema')
+let proyectos3 = document.getElementsByClassName('proyectos-personales')[0]
+let habilidades = document.getElementsByClassName('habilidades')[0]
+let habilidades2 = document.getElementsByClassName('h2-habilidades')
+let sobreMi = document.getElementsByClassName('sobre-mi')[0]
+let sobreMi2 = document.getElementsByClassName('cajas')[0]
+let contacto = document.getElementsByClassName('contacto')[0]
+let footer = document.getElementsByClassName('section-footer')[0]
+
+
+colorPagina.addEventListener('click', cambiarTema)
+
+function cambiarTema(){
+    if(colorPagina.textContent == '🌞'){
+        body.style.color = '#f5f7fa'
+        inicio.style.backgroundColor = '#f5f7fa'
+        
+        for(let elementos of letrasHero){
+            elementos.style.color = "#212121"
+        }
+        
+        proyectos1.style.backgroundColor = '#f5f7fa'
+    
+        for(let elem of proyectos2){
+            elem.style.color = "#212121"
+        }
+    
+        proyectos3.style.backgroundColor = '#f5f7fa'
+        habilidades.style.backgroundColor = '#f5f7fa'
+        sobreMi.style.backgroundColor = '#f5f7fa'
+        sobreMi2.style.backgroundColor = '#f5f7fa'
+        contacto.style.backgroundColor = '#f5f7fa'
+    
+        colorPagina.textContent = '🌙'
+    }
+    else{
+        body.style.color = '#191D2B'
+        inicio.style.backgroundColor = '#191D2B'
+        
+        for(let elementos of letrasHero){
+            elementos.style.color = "white"
+        }
+        
+        proyectos1.style.backgroundColor = '#191D2B'
+    
+        for(let elem of proyectos2){
+            elem.style.color = "white"
+        }
+    
+        proyectos3.style.backgroundColor = '#191D2B'
+    
+        habilidades.style.backgroundColor = '#191D2B'
+        sobreMi.style.backgroundColor = '#191D2B'
+        sobreMi2.style.backgroundColor = '#191D2B'
+        contacto.style.backgroundColor = '#191D2B'
+        footer.style.backgroundColor = '#191D2B'
+    
+        colorPagina.textContent = '🌞'
+    }
+
+}
+
+//------ANIMACION HERO-------
 let presentacionHero = document.getElementById('presentacionHero');
 let presentacion = 'Desarrollador viviendo en Entre Ríos, Argentina.';
 let velocidad = 50;
@@ -16,7 +85,7 @@ function animacionEscribir(){
 //Acá se invoca la funcion para animar el texto del hero
 animacionEscribir();
 
-//ALERTA AL ENVIAR INFORMACION EN FORMULARIO CONTACTO
+//-----ALERTA AL ENVIAR INFORMACION EN FORMULARIO CONTACTO-------
 let btnEnviar = document.getElementById('btnEnviar');
 
 //Evento al hacer click en enviar en el formulario de contacto
@@ -26,3 +95,5 @@ btnEnviar.addEventListener('click', mensajeAlContactarse)
 function mensajeAlContactarse(){
     alert('Ups 😓 Esta funcionalidad todavia no está disponible')
 }
+
+
